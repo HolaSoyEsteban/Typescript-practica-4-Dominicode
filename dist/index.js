2f533a3c3518fd29b4df7f4033aab54bdd8eca7a
Esteban Cardozo <cardozo.esteban@gmail.com>
2023-05-23 16:50:18 -0300

    Practica 4, Finalizada

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,29 @@
 "use strict";
-console.log('Hello World!');
+// Generics
+// type dataType = Person | Product;
+class DataCollection {
+    constructor() {
+        this.items = [];
+    }
+    addItem(newItem) {
+        this.items.push(newItem);
+    }
+    getItems() {
+        console.log(`List of items`, JSON.stringify(this.items));
+    }
+    getNames() {
+        return this.items.map((item) => item.name);
+    }
+    getItemById(id) {
+        return this.items.find((item) => item.id === id);
+    }
+}
+const productCollection = new DataCollection();
+const newData2 = {
+    id: 2,
+    name: 'Beer',
+    price: 2.5
+};
+productCollection.addItem(newData2);
+productCollection.getItems();
+const personCollection = new DataCollection();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,56 @@
-console.log('Hello World!');
+// Generics
+
+interface Person {
+    id: number;
+    name: string;
+}
+
+interface Employee extends Person {
+    role: string;
+}
+
+interface Product {
+    id: number;
+    name: string;
+    price: number;
+}
+
+interface Data<T> {
+    addItem(newItem: T): void;
+}
+
+// type dataType = Person | Product;
+
+class DataCollection<T extends { id: number, name: string }> implements Data<T> {
+    private items: T[] = [];
+
+    addItem(newItem: T): void {
+        this.items.push(newItem);
+    }
+
+    getItems(): void {
+        console.log(`List of items`, JSON.stringify(this.items));
+    }
+
+    getNames(): string[] {
+        return this.items.map((item) => item.name);
+    }
+
+    getItemById(id: number): T | undefined {
+        return this.items.find((item: T) => item.id === id);
+    } 
+}
+
+const productCollection = new DataCollection<Product>();
+
+const newData2 = {
+    id: 2,
+    name: 'Beer',
+    price: 2.5
+}
+
+productCollection.addItem(newData2);
+
+productCollection.getItems();
+
+const personCollection = new DataCollection<Employee>();
